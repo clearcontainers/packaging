c38a5b8546cdfe4cb35898e66b2fc4e58d411424
Erick Cardona <erick.cardona.ruiz@intel.com>
2017-09-12 17:49:16 +0000

    proxy: Fix binary --version displayed name.
    This commit ensure that the cc-proxy binary will display the correct
    version and commit hash  when calling it with the --version parameter.
    
    Fixes #107
    
    Signed-off-by: Erick Cardona <erick.cardona.ruiz@intel.com>

--- proxy/update_proxy.sh
+++ proxy/update_proxy.sh
@@ -11,6 +11,11 @@ AUTHOR_EMAIL=${AUTHOR_EMAIL:-$(git config user.email)}
 
 source ../versions.txt
 VERSION=${1:-$cc_proxy_version}
+# 3.0.0-beta.X format cause errors while packaging.
+# Remove the dash in order to be able to build. The
+# original string will remain in the packages and
+# in the binary's version.
+DASHLESS_VERSION=$(echo $VERSION | tr -d '-')
 
 # If we are providing the branch or hash to build we'll take version as the hashtag
 [ -n "$1" ] && hash_tag=$VERSION || hash_tag=$cc_proxy_hash
@@ -20,13 +25,13 @@ if [[ ${VERSION::1} =~ [a-z] ]]; then
     ORIGINAL_VERSION=$VERSION
     VERSION=1${VERSION:1}
 fi
-VERSION_DEB_TRANSFORM=$(echo $VERSION | tr -d '-')
 
 OBS_PUSH=${OBS_PUSH:-false}
 OBS_PROXY_REPO=${OBS_PROXY_REPO:-home:clearcontainers:clear-containers-3-staging/cc-proxy}
 : ${OBS_APIURL:=""}
 
-if [ $OBS_APIURL != '' ]; then
+# This allows to point to internal/private OBS instance
+if [ "$OBS_APIURL" != "" ]; then
     APIURL="-A ${OBS_APIURL}"
 else
     APIURL=""
@@ -49,9 +54,23 @@ function changelog_update {
     rm debian.changelog-bk
 }
 changelog_update $VERSION
-sed -e "s/@VERSION@/$VERSION/" cc-proxy.spec-template > cc-proxy.spec
-sed -e "s/@VERSION_DEB_TRANSFORM@/$VERSION_DEB_TRANSFORM/g;" -e "s/@HASH_TAG@/$short_hashtag/g;" cc-proxy.dsc-template > cc-proxy.dsc
-sed -e "s/@VERSION_DEB_TRANSFORM@/$VERSION_DEB_TRANSFORM/g;" -e "s/@HASH_TAG@/$short_hashtag/g;" debian.control-template > debian.control
+
+RELEASE=$(($(cat release) + 1))
+echo $RELEASE > release
+
+sed -e "s/@DASHLESS_VERSION@/$DASHLESS_VERSION/g" \
+    -e "s/@RELEASE@/$RELEASE/g" \
+    -e "s/@HASH@/$short_hashtag/g" \
+    -e "s/@VERSION@/${VERSION}-${short_hashtag}/g" cc-proxy.spec-template > cc-proxy.spec
+
+sed -e "s/@VERSION@/${VERSION}-${short_hashtag}/" debian.rules-template > debian.rules
+
+sed -e "s/@DASHLESS_VERSION@/$DASHLESS_VERSION/g" \
+    -e "s/@VERSION@/$VERSION/g" \
+    -e "s/@HASH@/$short_hashtag/g" \
+    -e "s/@RELEASE@/$RELEASE/g" cc-proxy.dsc-template > cc-proxy.dsc
+
+sed -e "s/@DASHLESS_VERSION@/$DASHLESS_VERSION/g" debian.control-template > debian.control
 
 if [ -z "$ORIGINAL_VERSION" ]; then
     sed "s/@VERSION@/$VERSION/g;" _service-template > _service
@@ -67,16 +86,17 @@ then
     temp=$(basename $0)
     TMPDIR=$(mktemp -d -t ${temp}.XXXXXXXXXXX) || exit 1
     osc $APIURL co "$OBS_PROXY_REPO" -o $TMPDIR
+
     mv cc-proxy.spec \
        cc-proxy.dsc \
        debian.control \
+       debian.rules \
         _service \
         $TMPDIR
     rm $TMPDIR/*.patch
     [ -f $TMPDIR/debian.series ] && rm $TMPDIR/debian.series || :
     cp debian.changelog \
         debian.compat \
-        debian.rules \
         *.patch \
         $TMPDIR
     [ -f debian.series ] && cp debian.series $TMPDIR || :
