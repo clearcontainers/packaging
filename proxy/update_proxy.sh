d8736fe7f7136cf1b0ac299fe7a87a3b3285892d
Erick Cardona <erick.cardona.ruiz@intel.com>
2017-10-20 15:31:15 +0000

    proxy: Update setup scripts
    this commit refactor the setup script allowing new features,
    needed for some use cases and adding a better user experience.
    
    Fixes #166
    
    Signed-off-by: Erick Cardona <erick.cardona.ruiz@intel.com>

--- proxy/update_proxy.sh
+++ proxy/update_proxy.sh
@@ -3,114 +3,83 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 #
 # Automation script to create specs to build cc-proxy
-# Default: Build is the one specified in file configure.ac
-# located at the root of the repository.
-set -x
-AUTHOR=${AUTHOR:-$(git config user.name)}
-AUTHOR_EMAIL=${AUTHOR_EMAIL:-$(git config user.email)}
+set -e
 
 source ../versions.txt
+source ../scripts/pkglib.sh
+
+SCRIPT_NAME=$0
+SCRIPT_DIR=$(dirname $0)
+PKG_NAME="cc-proxy"
 VERSION=$cc_proxy_version
+RELEASE=$(cat release)
+APPORT_HOOK="source_cc-proxy.py"
 
-# If we are providing the branch or hash to build, assign it to OBS_REVISION
-if [ -n "$1" ]; then
-     OBS_REVISION=$1
+BUILD_DISTROS=(Fedora_26 xUbuntu_16.04)
 
-     # Validate input is alphanumeric, commit ID
-     # If a commit ID is provided, override versions.txt one
-     if [[ "$OBS_REVISION" =~ ^[a-zA-Z0-9][-a-zA-Z0-9]{0,40}[a-zA-Z0-9]$  ]]; then
-         hash_tag=$OBS_REVISION
-     else
-         hash_tag=$cc_proxy_hash
-     fi
-else
-         hash_tag=$cc_proxy_hash
-fi
-short_hashtag="${hash_tag:0:7}"
+GENERATED_FILES=(cc-proxy.spec cc-proxy.dsc debian.control debian.rules _service)
+STATIC_FILES=(debian.changelog debian.compat)
 
-OBS_PUSH=${OBS_PUSH:-false}
-OBS_PROXY_REPO=${OBS_PROXY_REPO:-home:clearcontainers:clear-containers-3-staging/cc-proxy}
-OBS_APIURL=${OBS_APIURL:-""}
+COMMIT=false
+BRANCH=false
+LOCAL_BUILD=false
+OBS_PUSH=false
+VERBOSE=false
 
-# This allows to point to internal/private OBS instance
-if [ "$OBS_APIURL" != "" ]; then
-    APIURL="-A ${OBS_APIURL}"
-else
-    APIURL=""
-fi
+# Parse arguments
+cli "$@"
 
-echo "Running: $0 $@"
-echo "Update cc-proxy $VERSION: ${hash_tag:0:7}"
+[ "$VERBOSE" == "true" ] && set -x || true
+PROJECT_REPO=${PROJECT_REPO:-home:clearcontainers:clear-containers-3-staging/cc-proxy}
+[ -n "$APIURL" ] && APIURL="-A ${APIURL}" || true
 
-function changelog_update {
-    d=$(date +"%a, %d %b %Y %H:%M:%S %z")
-    git checkout debian.changelog
-    cp debian.changelog debian.changelog-bk
-    cat <<< "cc-proxy ($VERSION) stable; urgency=medium
+# Generate specs using templates
+function template()
+{
+    sed -e "s/@VERSION@/$VERSION/g" \
+        -e "s/@RELEASE@/$RELEASE/g" \
+        -e "s/@HASH@/$short_hashtag/g" \
+        -e "s/@VERSION_STRING@/${VERSION}+git.${short_hashtag}/g" cc-proxy.spec-template > cc-proxy.spec
 
-  * Update cc-proxy $VERSION ${hash_tag:0:7}
+    sed -e "s/@VERSION_STRING@/${VERSION}+git.${short_hashtag}/" debian.rules-template > debian.rules
 
- -- $AUTHOR <$AUTHOR_EMAIL>  $d
-" > debian.changelog
-    cat debian.changelog-bk >> debian.changelog
-    rm debian.changelog-bk
-}
-changelog_update $VERSION
+    sed -e "s/@VERSION@/$VERSION/g" \
+        -e "s/@HASH@/$short_hashtag/g" \
+        -e "s/@RELEASE@/$RELEASE/g" cc-proxy.dsc-template > cc-proxy.dsc
 
-RELEASE=$(($(cat release) + 1))
-echo $RELEASE > release
+    sed -e "s/@VERSION@/$VERSION/g" debian.control-template > debian.control
 
-sed -e "s/@VERSION@/$VERSION/g" \
-    -e "s/@RELEASE@/$RELEASE/g" \
-    -e "s/@HASH@/$short_hashtag/g" \
-    -e "s/@VERSION_STRING@/${VERSION}+git.${short_hashtag}/g" cc-proxy.spec-template > cc-proxy.spec
-
-sed -e "s/@VERSION_STRING@/${VERSION}+git.${short_hashtag}/" debian.rules-template > debian.rules
-
-sed -e "s/@VERSION@/$VERSION/g" \
-    -e "s/@HASH@/$short_hashtag/g" \
-    -e "s/@RELEASE@/$RELEASE/g" cc-proxy.dsc-template > cc-proxy.dsc
-
-sed -e "s/@VERSION@/$VERSION/g" debian.control-template > debian.control
-
-# If OBS_REVISION is not empty, which means a branch or commit ID has been passed as argument,
-# replace It as @REVISION@ it in the OBS _service file. Otherwise, use the VERSION variable,
-# which uses the version from versions.txt.
-# This will determine which source tarball will be retrieved from github.com
-if [ -n "$OBS_REVISION" ]; then
-    sed "s/@REVISION@/$OBS_REVISION/" _service-template > _service
-else
-    sed "s/@REVISION@/$VERSION/"  _service-template > _service
-fi
-
-# Update and package OBS
-if [ "$OBS_PUSH" = true ]
-then
-    temp=$(basename $0)
-    TMPDIR=$(mktemp -d -t ${temp}.XXXXXXXXXXX) || exit 1
-    osc $APIURL co "$OBS_PROXY_REPO" -o $TMPDIR
-
-    mv cc-proxy.spec \
-       cc-proxy.dsc \
-       debian.control \
-       debian.rules \
-        _service \
-        $TMPDIR
-    rm $TMPDIR/*.patch
-    [ -f $TMPDIR/debian.series ] && rm $TMPDIR/debian.series || :
-    cp debian.changelog \
-        debian.compat \
-        *.patch \
-        $TMPDIR
-
-    cp ../scripts/apport_hook.py $TMPDIR/source_cc-proxy.py
-
-    [ -f debian.series ] && cp debian.series $TMPDIR || :
-    cd $TMPDIR
-
-    if [ -e "go1.8.3.linux-amd64.tar.gz" ]; then
-        rm go*.tar.gz
+    # If OBS_REVISION is not empty, which means a branch or commit ID has been passed as argument,
+    # replace It as @REVISION@ it in the OBS _service file. Otherwise, use the VERSION variable,
+    # which uses the version from versions.txt.
+    # This will determine which source tarball will be retrieved from github.com
+    if [ -n "$OBS_REVISION" ]; then
+        sed "s/@REVISION@/$OBS_REVISION/" _service-template > _service
+    else
+        sed "s/@REVISION@/$VERSION/"  _service-template > _service
     fi
-    osc $APIURL addremove
-    osc $APIURL commit -m "Update cc-proxy $VERSION: ${hash_tag:0:7}"
+}
+
+verify
+echo "Verify succeed."
+get_git_info
+set_versions $cc_proxy_hash
+changelog_update $VERSION
+template
+
+if [ "$LOCAL_BUILD" == "true" ] && [ "$OBS_PUSH" == "true" ]
+then
+	die "--local-build and --push are mutually exclusive."
+
+elif [ "$LOCAL_BUILD" == "true" ]
+then
+	checkout_repo $PROJECT_REPO
+	local_build	
+elif [ "$OBS_PUSH" == "true" ]
+then
+	checkout_repo $PROJECT_REPO
+	obs_push "cc-proxy"
 fi
+echo "OBS working copy directory: ${OBS_WORKDIR}"
+
+
