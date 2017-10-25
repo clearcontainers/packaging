192974ecf055b893ac77597d977b85f0684c06af
Erick Cardona <erick.cardona.ruiz@intel.com>
2017-10-25 10:48:45 -0500

    kernel: Update setup scripts
    this commit refactor the setup script allowing new features,
    needed for some use cases and adding a better user experience.
    
    Fixes #166
    
    Signed-off-by: Erick Cardona <erick.cardona.ruiz@intel.com>

--- kernel/update_kernel.sh
+++ kernel/update_kernel.sh
@@ -3,80 +3,70 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 # Automation script to create specs to build clear containers kernel
-set -x
+set -e
+
+source ../versions.txt
+source ../scripts/pkglib.sh
+
+SCRIPT_NAME=$0
+SCRIPT_DIR=$(dirname $0)
+PKG_NAME="linux-container"
+VERSION=$clear_vm_kernel_version
+RELEASE=$(cat release)
+BUILD_DISTROS=(Fedora_26 xUbuntu_16.04)
 
 KR_REL=https://www.kernel.org/releases.json
 KR_SHA=https://cdn.kernel.org/pub/linux/kernel/v4.x/sha256sums.asc
 KR_LTS=4.9
 
-AUTHOR=${AUTHOR:-$(git config user.name)}
-AUTHOR_EMAIL=${AUTHOR_EMAIL:-$(git config user.email)}
+GENERATED_FILES=(linux-container.dsc linux-container.spec _service)
+STATIC_FILES=(debian.dirs debian.rules debian.compat debian.control debian.copyright patches-4.9.x/*.patch)
+OBS_CC_KERNEL_REPO=${OBS_CC_KERNEL_REPO:-home:clearcontainers:clear-containers-3-staging/$PKG_NAME}
 
-OBS_PUSH=${OBS_PUSH:-false}
-OBS_CC_KERNEL_REPO=${OBS_CC_KERNEL_REPO:-home:clearcontainers:clear-containers-3-staging/linux-container}
+COMMIT=false
+BRANCH=false
+LOCAL_BUILD=false
+OBS_PUSH=false
+VERBOSE=false
 
-VERSION=${1:-latest}
+# Parse arguments
+cli "$@"
 
-function changelog_update {
-    d=$(date -R)
-    cp debian.changelog debian.changelog-bk
-    cat <<< "linux-container ($VERSION-$next_release) stable; urgency=medium
-
-  * Update kernel $VERSION
-
- -- $AUTHOR <$AUTHOR_EMAIL>  $d
-" > debian.changelog
-
-    cat debian.changelog-bk >> debian.changelog
-    rm debian.changelog-bk
-}
-
-echo "Running: $0 $@"
-
-git checkout -- release debian.changelog
-last_release=$(< release)
-next_release=$(( $last_release + 1 ))
-echo ${next_release} > release
-
-if [ "${VERSION}" = "latest" ]
+[ "$VERBOSE" == "true" ] && set -x || true
+PROJECT_REPO=${PROJECT_REPO:-home:clearcontainers:clear-containers-3-staging/linux-container}
+[ -n "$APIURL" ] && APIURL="-A ${APIURL}" || true
+if [ "${VERSION}" == "latest" ]
 then
     VERSION=$(curl -L -s -f ${KR_REL} | grep "${KR_LTS}" | grep version | cut -f 4 -d \" | grep "^${KR_LTS}")
 fi
 
 kernel_sha256=$(curl -L -s -f ${KR_SHA} | awk '/linux-'${VERSION}'.tar.xz/ {print $1}')
 
-echo "Update linux-container to: $VERSION-$next_release"
+# Generate specs using templates
+function template()
+{
+    sed "s/\@VERSION\@/$VERSION/g; s/\@RELEASE\@/$RELEASE/g" linux-container.spec-template > linux-container.spec
+    sed "s/\@VERSION\@/$VERSION/g; s/\@RELEASE\@/$RELEASE/g" linux-container.dsc-template > linux-container.dsc
+    sed "s/\@VERSION\@/$VERSION/g; s/\@KERNEL_SHA256\@/$kernel_sha256/g" _service-template > _service
+}
 
-changelog_update ${VERSION}
+verify
+echo "Verify succeed."
+get_git_info
+changelog_update $VERSION
+template
 
-sed "s/\@VERSION\@/${VERSION}/g; s/\@RELEASE\@/${next_release}/g" linux-container.spec-template > linux-container.spec
-sed "s/\@VERSION\@/${VERSION}/g; s/\@RELEASE\@/${next_release}/g" linux-container.dsc-template > linux-container.dsc
-sed "s/\@VERSION\@/${VERSION}/g; s/\@KERNEL_SHA256\@/${kernel_sha256}/g" _service-template > _service
-
-if [ $? = 0 ] && [ "$OBS_PUSH" = true ]
+if [ "$LOCAL_BUILD" == "true" ] && [ "$OBS_PUSH" == "true" ]
 then
-    temp=$(basename $0)
-    TMPDIR=$(mktemp -d -t ${temp}.XXXXXXXXXXX) || exit 1
-    osc co "$OBS_CC_KERNEL_REPO" -o $TMPDIR
-    mv linux-container.dsc \
-       linux-container.spec \
-        _service \
-        $TMPDIR
-    rm -f $TMPDIR/*.patch
-    rm -f $TMPDIR/linux-container_*
-    rm -f $TMPDIR/linux-*.tar.xz
-    rm -f $TMPDIR/debian.series
-    cp debian.changelog \
-        debian.dirs \
-        debian.rules \
-        debian.compat \
-        debian.control \
-        debian.copyright \
-        patches-4.9.x/*.patch \
-        $TMPDIR
-    cp kernel-config-4.9.x $TMPDIR/config
-    [ -f debian.series ] && cp debian.series $TMPDIR || :
-    cd $TMPDIR
-    osc addremove
-    osc commit -m "Update linux-container to: $VERSION-$next_release"
+    die "--local-build and --push are mutually exclusive."
+elif [ "$LOCAL_BUILD" == "true" ]
+then
+	checkout_repo $PROJECT_REPO
+	local_build
+
+elif [ "$OBS_PUSH" == "true" ]
+then
+	checkout_repo $PROJECT_REPO
+	obs_push "linux-container"
 fi
+echo "OBS working copy directory: ${OBS_WORKDIR}"
